3403b2724b492d981c15460f0213d1999bb0c2bf
1363v4 <louis-emmanuel.gires@evabssi.com>
2026-02-04 10:51:27 +0100

    dev

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from pathlib import Path
 
 from stario import Context, RichTracer, Stario, Writer, asset, at, data, JsonTracer
-from stario.html import H1, Body, Button, Div, Head, Html, Meta, P, Script, Title
+from stario.html import H1, Body, Button, Div, Head, Html, Meta, P, Script, Title, Link, Main, Button, Span, Progress, A, Ul, Li
 from stario.http.router import Router
 
 from watchfiles import run_process
@@ -12,14 +12,61 @@ def home_view():
     return Html(
         {"lang": "en"},
         Head(
-            Meta({"charset": "UTF-8"}),
+            Meta({'charset': "UTF-8"}),
             Meta(
                 {"name": "viewport", "content": "width=device-width, initial-scale=1"}
             ),
-            Title("Hello World - Stario App"),
-            Script({"type": "module", "src": "/static/" + asset("js/datastar.js")}),
+            Title("The Tao"),
+            Link({
+                'rel': "icon",
+                'href': f"/static/{asset('img/favicon.ico')}"
+            }),
+            Link({
+                'rel': "stylesheet",
+                'href': f"/static/{asset('css/site.css')}"
+            }),
+            Script({
+                'type': "module",
+                'src': f"/static/{asset('js/datastar.js')}"
+            })
         ),
         Body(
+            Main(
+                Div(
+                    {'id': "welcome"},
+                    Span("Hello"),
+                    Span("There")
+                ),
+                Div(
+                    Progress(
+                        {
+                            'max': 100,
+                            'value': 0,
+                        },
+                        "0%"
+                    ),
+                    P("text")
+                ),
+                Div(
+                    {'id': "welcome"},
+                    A(
+                        {'href': "https://checkout.revolut.com/pay/439edbe5-bda3-4616-aa07-7c2123f5514e"},
+                        Button("In EUR")
+                    ),
+                    A(
+                        {'href': "https://checkout.revolut.com/pay/439edbe5-bda3-4616-aa07-7c2123f5514e"},
+                        Button("In EUR")
+                    ),
+                ),
+                Div(
+                    {'id': "supporters"},
+                    P("😺 Supporters 😺"),
+                    Ul(
+                        Li("Michael Spanner")
+                    )
+
+                )
+            )
         ),
     )
 
